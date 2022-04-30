85ddd701fb65da96d2e69882ea21b75dff9354e6
Vincent Wong <vincentwwy168@gmail.com>
2022-05-01 01:18:45 +0800

    Add basic DAO

---
+++ course_app/app/src/main/java/edu/cuhk/courseproject/datatypes/NotePinDao.java
@@ -0,0 +1,25 @@
+package edu.cuhk.courseproject.datatypes;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface NotePinDao {
+    // todo add more queries as needed
+
+    @Query("SELECT * FROM note_pin")
+    List<NotePin> getAllPins();
+
+    @Query("SELECT * FROM note_pin WHERE latitude BETWEEN :fromLatitude AND :toLatitude AND longitude BETWEEN :fromLongitude AND :toLongitude")
+    List<NotePin> getAllPinsInArea(double fromLatitude, double fromLongitude, double toLatitude, double toLongitude);
+
+    @Insert
+    void insertPins(NotePin... pins);
+
+    @Delete
+    void deletePin(NotePin pin);
+}

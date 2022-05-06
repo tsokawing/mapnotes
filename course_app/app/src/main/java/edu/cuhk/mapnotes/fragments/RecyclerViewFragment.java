2b3459db8496369caf8c6413a3c6006ec3025077
Colin <tsokawing@protonmail.com>
2022-05-06 11:02:24 +0800

    Change swipe right to swipe left to remove note

--- course_app/app/src/main/java/edu/cuhk/mapnotes/fragments/RecyclerViewFragment.java
+++ course_app/app/src/main/java/edu/cuhk/mapnotes/fragments/RecyclerViewFragment.java
@@ -93,7 +93,7 @@ public class RecyclerViewFragment extends Fragment {
     }
 
     void useSwipeToDelete(RecyclerView recyclerView, PinsAdapter adapter) {
-        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
             @Override
             public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
                 // This method is called when the item is moved.

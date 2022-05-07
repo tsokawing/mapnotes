16d2f4c1d67cfef1324586cff5496082388aeedb
Vincent Wong <vincentwwy168@gmail.com>
2022-05-07 16:47:41 +0800

    Refine UI of notes

--- course_app/app/src/main/java/edu/cuhk/mapnotes/activities/NotesActivity.java
+++ course_app/app/src/main/java/edu/cuhk/mapnotes/activities/NotesActivity.java
@@ -1,6 +1,8 @@
 package edu.cuhk.mapnotes.activities;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -11,6 +13,7 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -23,6 +26,7 @@ import java.util.Objects;
 import edu.cuhk.mapnotes.databinding.ActivityNotesBinding;
 import edu.cuhk.mapnotes.R;
 import edu.cuhk.mapnotes.datatypes.NoteEntry;
+import edu.cuhk.mapnotes.util.HelpButtonOnClickListener;
 
 public class NotesActivity extends AppCompatActivity {
 
@@ -31,6 +35,8 @@ public class NotesActivity extends AppCompatActivity {
     private boolean isEditing = false;
 
     private int noteEntryUid;
+
+    private EditText inputRenameTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,6 +76,47 @@ public class NotesActivity extends AppCompatActivity {
                 } else {
                     startEditText();
                 }
+            }
+        });
+
+        // edit title
+        // first prepare the dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.renaming_note_title);
+        EditText inputRenameTitle = new EditText(this);
+        inputRenameTitle.setInputType(InputType.TYPE_CLASS_TEXT);
+        // should have already loaded a valid note
+        if (noteEntryUid >= 0) {
+            NoteEntry entry = MapsActivity.noteDatabase.noteEntryDao().getNoteEntry(noteEntryUid);
+            inputRenameTitle.setText(entry.noteTitle);
+        }
+        builder.setView(inputRenameTitle);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (noteEntryUid >= 0) {
+                    // put the updated title back to the DB
+                    NoteEntry noteEntry = MapsActivity.noteDatabase.noteEntryDao().getNoteEntry(noteEntryUid);
+                    noteEntry.noteTitle = inputRenameTitle.getText().toString();
+                    MapsActivity.noteDatabase.noteEntryDao().updateNoteEntry(noteEntry);
+                    properlySetToolbarTitle(noteEntry.noteTitle);
+                }
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        AlertDialog properDialog = builder.create();
+
+        // then link the dialog to the UI element
+        FloatingActionButton fabEditTitle = binding.fabEditRenameTitle;
+        fabEditTitle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                properDialog.show();
             }
         });
     }

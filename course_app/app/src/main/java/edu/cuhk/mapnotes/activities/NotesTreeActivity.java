242ae4f89f78b8d119530f534b544a3ae121f8b9
Colin Tso <tsokawing@protonmail.com>
2022-05-19 12:45:22 +0800

    Support both search by tag/pin

--- course_app/app/src/main/java/edu/cuhk/mapnotes/activities/NotesTreeActivity.java
+++ course_app/app/src/main/java/edu/cuhk/mapnotes/activities/NotesTreeActivity.java
@@ -1,6 +1,14 @@
 package edu.cuhk.mapnotes.activities;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.SearchView;
 
 import com.amrdeveloper.treeview.TreeNode;
@@ -8,6 +16,8 @@ import com.amrdeveloper.treeview.TreeViewAdapter;
 import com.amrdeveloper.treeview.TreeViewHolderFactory;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -19,6 +29,7 @@ import edu.cuhk.mapnotes.adapters.NotesTreeViewHolder;
 import edu.cuhk.mapnotes.databinding.ActivityNotesTreeBinding;
 import edu.cuhk.mapnotes.datatypes.NoteEntry;
 import edu.cuhk.mapnotes.datatypes.NotePin;
+import edu.cuhk.mapnotes.datatypes.NoteTag;
 
 public class NotesTreeActivity extends AppCompatActivity {
 
@@ -28,6 +39,8 @@ public class NotesTreeActivity extends AppCompatActivity {
 
     RecyclerView notesTreeRecyclerView;
     TreeViewAdapter treeViewAdapter;
+
+    private boolean isSearchingByName = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,8 +59,52 @@ public class NotesTreeActivity extends AppCompatActivity {
         treeViewAdapter = new TreeViewAdapter(factory);
         notesTreeRecyclerView.setAdapter(treeViewAdapter);
 
-        loadAllNotes();
+        setupToolBar();
         setUpSearch();
+
+        loadAllNotesByName();
+    }
+
+    private void setupToolBar() {
+        Toolbar toolBar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolBar);
+        getSupportActionBar().setTitle("Notes Explorer");
+
+        toolBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_tree, menu);
+        MenuItem itemSwitch = menu.findItem(R.id.switch_action_bar);
+        itemSwitch.setActionView(R.layout.switch_tree_content);
+
+        final SwitchCompat sw = menu.findItem(R.id.switch_action_bar).getActionView().findViewById(R.id.treeSwitch);
+
+        sw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                SearchView search = findViewById(R.id.notestree_search_widget);
+                search.setQuery("", false);
+
+                ImageView switchIcon = findViewById(R.id.tree_switch_icon);
+
+                if (isSearchingByName) {
+                    switchIcon.setImageResource(R.drawable.ic_baseline_local_offer_24);
+                    loadAllNotesByTag();
+                } else {
+                    switchIcon.setImageResource(R.drawable.ic_baseline_location_on_24);
+                    loadAllNotesByName();
+                }
+                isSearchingByName = !isSearchingByName;
+            }
+        });
+        return true;
     }
 
     private void setUpSearch() {
@@ -61,17 +118,26 @@ public class NotesTreeActivity extends AppCompatActivity {
             @Override
             public boolean onQueryTextChange(String newText) {
                 if (newText.length() > 0) {
-                    filterNotes(newText);
+                    if (isSearchingByName) {
+                        filterNotesByName(newText);
+                    } else {
+                        filterNotesByTag(newText);
+                    }
+
                     treeViewAdapter.expandAll();
                 } else {
-                    loadAllNotes();
+                    if (isSearchingByName) {
+                        loadAllNotesByName();
+                    } else {
+                        loadAllNotesByTag();
+                    }
                 }
                 return false;
             }
         });
     }
 
-    private void loadAllNotes() {
+    private void loadAllNotesByName() {
         List<TreeNode> roots = new ArrayList<>();
 
         notePins = MapsActivity.noteDatabase.notePinDao().getAllPins();
@@ -88,7 +154,7 @@ public class NotesTreeActivity extends AppCompatActivity {
         treeViewAdapter.updateTreeNodes(roots);
     }
 
-    private void filterNotes(String keyword) {
+    private void filterNotesByName(String keyword) {
         List<TreeNode> roots = new ArrayList<>();
 
         for (NotePin notePin : notePins) {
@@ -104,6 +170,56 @@ public class NotesTreeActivity extends AppCompatActivity {
                 roots.add(root);
             } else {
                 // Pin doesn't contain keyword, check notes inside
+                for (NoteEntry noteEntry : noteEntries) {
+                    if (noteEntry.noteTitle.toLowerCase().contains(keyword.toLowerCase())) {
+                        root.addChild(new TreeNode(noteEntry, R.layout.list_item_file));
+                    }
+                }
+                // Only add pin if there's at least one note contains keyword
+                if (!root.getChildren().isEmpty()) {
+                    roots.add(root);
+                }
+            }
+        }
+
+        treeViewAdapter.notifyDataSetChanged();
+        treeViewAdapter.updateTreeNodes(roots);
+    }
+
+    private void loadAllNotesByTag() {
+        List<TreeNode> roots = new ArrayList<>();
+
+        List<NoteTag> allTags = MapsActivity.noteDatabase.noteTagDao().getAllPossibleTags();
+        for (NoteTag tag : allTags) {
+            List<NoteEntry> taggedNotes = MapsActivity.noteDatabase.noteTagDao().getAllNoteEntriesUsingThisTag(tag.uid);
+            TreeNode root = new TreeNode(tag, R.layout.list_item_file);
+            for (NoteEntry taggedNote : taggedNotes) {
+                root.addChild(new TreeNode(taggedNote, R.layout.list_item_file));
+            }
+            roots.add(root);
+        }
+
+        treeViewAdapter.notifyDataSetChanged();
+        treeViewAdapter.updateTreeNodes(roots);
+    }
+
+    private void filterNotesByTag(String keyword) {
+        List<TreeNode> roots = new ArrayList<>();
+        List<NoteTag> allTags = MapsActivity.noteDatabase.noteTagDao().getAllPossibleTags();
+
+        for (NoteTag noteTag : allTags) {
+            List<NoteEntry> noteEntries = MapsActivity.noteDatabase.noteTagDao().getAllNoteEntriesUsingThisTag(noteTag.uid);
+            TreeNode root = new TreeNode(noteTag, R.layout.list_item_file);
+
+            // Filter here
+            if (noteTag.tagName.toLowerCase().contains(keyword.toLowerCase())) {
+                // Tags contains keyword, add all notes of this tag
+                for (NoteEntry noteEntry : noteEntries) {
+                    root.addChild(new TreeNode(noteEntry, R.layout.list_item_file));
+                }
+                roots.add(root);
+            } else {
+                // Tag doesn't contain keyword, check notes inside
                 for (NoteEntry noteEntry : noteEntries) {
                     if (noteEntry.noteTitle.toLowerCase().contains(keyword.toLowerCase())) {
                         root.addChild(new TreeNode(noteEntry, R.layout.list_item_file));
--- course_app/app/src/main/java/edu/cuhk/mapnotes/adapters/NotesTreeViewHolder.java
+++ course_app/app/src/main/java/edu/cuhk/mapnotes/adapters/NotesTreeViewHolder.java
@@ -15,6 +15,7 @@ import edu.cuhk.mapnotes.R;
 import edu.cuhk.mapnotes.activities.NotesActivity;
 import edu.cuhk.mapnotes.datatypes.NoteEntry;
 import edu.cuhk.mapnotes.datatypes.NotePin;
+import edu.cuhk.mapnotes.datatypes.NoteTag;
 
 public class NotesTreeViewHolder extends TreeViewHolder {
 
@@ -38,10 +39,13 @@ public class NotesTreeViewHolder extends TreeViewHolder {
         super.bindTreeNode(node);
         // Here you can bind your node and check if it selected or not
 
-        // A node can be a pin or a note entry, check and render list accordingly
+        // A node can be a pin, a tag or a note entry, check and render list accordingly
         if (node.getValue() instanceof NotePin) {
             fileName.setText(((NotePin) node.getValue()).pinName);
             fileTypeIcon.setImageResource(R.drawable.ic_baseline_location_on_24);
+        } else if (node.getValue() instanceof NoteTag) {
+            fileName.setText(((NoteTag) node.getValue()).tagName);
+            fileTypeIcon.setImageResource(R.drawable.ic_baseline_local_offer_24);
         } else if (node.getValue() instanceof NoteEntry) {
             fileName.setText(((NoteEntry) node.getValue()).noteTitle);
             fileTypeIcon.setImageResource(R.drawable.ic_baseline_insert_drive_file_24);
